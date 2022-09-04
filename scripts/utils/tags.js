19c0b06351c5ab645f732fb6e0c2044406e795d3
PalaciosCaroline <caroline2807@yahoo.fr>
2022-09-04 20:55:05 +0200

    suppression des datalist

--- scripts/utils/tags.js
+++ scripts/utils/tags.js
@@ -1,10 +1,5 @@
 import {recipes} from '../data/recipes.js';
 
-const dataIngredients = document.getElementById('dataIngredients');
-const dataAppliance = document.getElementById('dataAppliance');
-const dataUstensils = document.getElementById('dataUstensils');
-
-// export function getListsort(recipes){
     let ingredientsArray = [];
     let applianceArray = [];
     let ustensilsArray = [];
@@ -19,20 +14,8 @@ const dataUstensils = document.getElementById('dataUstensils');
     applianceArray = [...new Set(applianceArray)].sort();
     ustensilsArray = [...new Set(ustensilsArray)].sort();
 
-    console.log( ingredientsArray);
-    // return (ingredientsArray, applianceArray, ustensilsArray);
-// }
+const ingredientsUl = document.getElementById('ingredientsUl');
+const applianceUlUl = document.getElementById('applianceUlUl');
+const ustensilsUl = document.getElementById('ustensilsUl');
 
-// ingredientsArray.forEach(item => dataIngredients.option.value == item);
-// applianceArray.forEach(appliance => dataAppliance.option.value == appliance);
-// ustensilsArray.forEach(item => dataUstensils.option.value == item);
-
-
-let Ingredientslist = document.getElementById('ingredientsUl');
-
-ingredientsArray.forEach(function(item){
-   let option = document.createElement('option');
-   option.value = item;
-   Ingredientslist.appendChild(option);
-});
 
