bc71f70a9a006d9298970212059dca7f84608cfe
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-04 22:18:46 +0200

    nettoyage

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -1,6 +1,6 @@
 import {recipes} from '../data/recipes.js';
 import {displayRecipes} from '../factories/buildCard.js';
-import {noRecipeAlert, removeNoRecipeAlert} from '../factories/header.js';
+import {isAlert, noRecipeAlert, removeNoRecipeAlert} from '../factories/header.js';
 import {boxresultsUl} from '../factories/buildListForTag.js';
 import {buildUlListfilter, getIngredientsList, getApplianceList, getUstensilsList} from '../factories/buildListForTag.js';
 import {displayTag} from '../factories/buildtag.js';
@@ -14,9 +14,11 @@ let datasProxy = new Proxy(datas, {
         switch(key) {
             case 'filtredRecipes': 
                 if ( datasProxy.filtredRecipes.length == 0){
-                    noRecipeAlert();
-                } else {
-                    removeNoRecipeAlert();
+                        noRecipeAlert();
+                    } else {
+                        if(isAlert){
+                        removeNoRecipeAlert();
+                        }
                     // afficher les recettes
                     displayRecipes(value);
                     //mettre a jour la liste des ingredients
