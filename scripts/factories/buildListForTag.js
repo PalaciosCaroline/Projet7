83360967bc0d82d236107ae9a516006c9f9809f7
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-05 12:54:59 +0200

    gestion open et close de list de choix de tag

--- scripts/factories/buildListForTag.js
+++ scripts/factories/buildListForTag.js
@@ -51,3 +51,70 @@ export function getUstensilsList(recipes){
     buildUlListfilter(ustensilsArray, ustensilsUl);
     })
 }
+
+function chevronUp(item){
+    item.classList.remove('fa-chevron-down');
+    item.classList.add('fa-chevron-up');
+}
+
+function chevronDown(item){
+    item.classList.remove('fa-chevron-up');
+    item.classList.add('fa-chevron-down');
+}
+
+function openListTagChoice(itemOpen, indexOpen, itemclose1, indexclose1, itemclose2, indexclose2){
+    chevronUp(itemOpen);
+    boxresults[indexOpen].classList.add('open');
+    if(btnList[indexclose1].classList.contains('isOpen')){
+        closeListTagChoice(itemclose1, indexclose1);
+    }else if(btnList[indexclose2].classList.contains('isOpen')) {
+        closeListTagChoice(itemclose2, indexclose2);
+    }
+}
+
+function closeListTagChoice(item, index){
+    boxresults[index].querySelector('input').value = '';
+    chevronDown(item);
+    btnList[index].classList.remove('isOpen');
+    boxresults[index].classList.remove('open');
+}
+
+export function getOpenListTag() {
+
+    btnList.forEach(item => item.addEventListener('click', () => {
+        if(!item.classList.contains('isOpen')){
+            item.classList.add('isOpen');
+            if(item.classList.contains('btn_ingredients')){
+                    openListTagChoice(icons[0], 0, icons[1], 1, icons[2], 2);
+            }else if(item.classList.contains('btn_appliance')){
+                    openListTagChoice(icons[1], 1, icons[0], 0, icons[2], 2);
+            } else if(item.classList.contains('btn_ustensils')){
+                    openListTagChoice(icons[2], 2, icons[1], 1, icons[0], 0);
+            }
+        } else if(item.classList.contains('isOpen')){
+            if(item.classList.contains('btn_ingredients')){
+                closeListTagChoice(icons[0], 0);
+                inputList[0].value = '';
+            } else if(item.classList.contains('btn_appliance')){
+                closeListTagChoice(icons[1], 1);
+                inputList[1].value = '';
+            } else if(item.classList.contains('btn_ustensils')){
+                closeListTagChoice(icons[2], 2);
+                inputList[2].value = '';
+            }
+        }
+    }));
+
+    inputList.forEach(item => item.addEventListener('focusin', (e) => {
+        if(e.target.parentNode.classList.contains('label_ingredients')){
+            btnList[0].classList.add('isOpen');
+            openListTagChoice(icons[0], 0, icons[1], 1,icons[2], 2);
+        } else if(e.target.parentNode.classList.contains('label_appliance')) {
+            btnList[1].classList.add('isOpen');
+            openListTagChoice(icons[1], 1,icons[0], 0,icons[2], 2);
+        } else if(e.target.parentNode.classList.contains('label_ustensils')) {
+            btnList[2].classList.add('isOpen');
+            openListTagChoice(icons[2], 2, icons[1], 1, icons[0], 0);
+        }
+    }));
+}
