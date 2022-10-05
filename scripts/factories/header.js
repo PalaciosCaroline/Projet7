1db2e39ee9e66ccc91abfc231ee532e4f0df6456
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-05 13:33:31 +0200

    message norecipe

--- scripts/factories/header.js
+++ scripts/factories/header.js
@@ -5,3 +5,17 @@
 // <h1 class=""><img class= "d-block mx-auto" src="./assets/Les petits plats.png" alt="Les petits plats"></h1>`
 // return header;
 // }
+
+export let isAlert;
+
+export function noRecipeAlert(){
+    document.querySelector('.input_search_bar').classList.add('alert');
+    document.querySelector('#fieldset_searchbar').classList.add('alert');
+    return isAlert = true;
+}
+
+export function removeNoRecipeAlert(){
+    document.querySelector('.input_search_bar').classList.remove('alert');
+    document.querySelector('#fieldset_searchbar').classList.remove('alert');
+    return isAlert = false;
+}
