d98258ec1fdfe8111ba7eaf13430f923c459b681
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-07 15:41:23 +0200

    Refactorisation

--- scripts/factories/buildListForTag.js
+++ scripts/factories/buildListForTag.js
@@ -17,32 +17,6 @@ export function buildUlListfilter(ArrayList, containerList) {
     })) 
 }
 
-export function getIngredientsList(recipes){
-    let ingredientsArray = [];
-    recipes.forEach((recipe) => {
-        recipe.ingredients.map((element) => ingredientsArray.push(element.ingredient.toLowerCase()));
-    ingredientsArray = [...new Set(ingredientsArray)].sort();
-    buildUlListfilter(ingredientsArray, boxresultsUl[0]);
-    })
-}
-
-export function getApplianceList(recipes){
-    let applianceArray = [];
-    recipes.forEach((recipe) => {
-        applianceArray.push(recipe.appliance.toLowerCase());
-    applianceArray = [...new Set(applianceArray)].sort();
-    buildUlListfilter(applianceArray, boxresultsUl[1]);
-    })
-}
-export function getUstensilsList(recipes){
-    let ustensilsArray = [];
-    recipes.forEach((recipe) => {
-    recipe.ustensils.map((element) => ustensilsArray.push(element.toLowerCase()));
-    ustensilsArray = [...new Set(ustensilsArray)].sort();
-    buildUlListfilter(ustensilsArray, boxresultsUl[2]);
-    })
-}
-
 function chevronUp(item){
     item.classList.remove('fa-chevron-down');
     item.classList.add('fa-chevron-up');
