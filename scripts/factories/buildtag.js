71b37ee1cdb145d6b893e30c37661af7efd83486
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-07 17:58:44 +0200

    Rendu maquette

--- scripts/factories/buildtag.js
+++ scripts/factories/buildtag.js
@@ -2,16 +2,17 @@
 export function buildTagChoice(element, typeclass){
     const tagelement = document.createElement('div');
     tagelement.className = `tagChoice ${typeclass}`;
-    tagelement.id = element;
     const textTag = document.createElement('span');
     textTag.className = 'textTag';
     textTag.innerHTML = element;
-    const btnClose = document.createElement('img');
+    const btnClose = document.createElement('button');
     btnClose.className = 'btnClose';
-    btnClose.src = './assets/close.svg'
-    btnClose.setAttribute('data-element', element);
+    btnClose.id = element;
+    const btnIcon = document.createElement('icon');
+    btnIcon.className = 'fa-regular fa-circle-xmark';
     tagelement.appendChild(textTag);
     tagelement.appendChild(btnClose);
+    btnClose.appendChild(btnIcon);
     return tagelement;
 }
 
