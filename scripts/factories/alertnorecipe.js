bff9871ae882630559dff96c4db298f0a2ae6d13
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-12 18:20:12 +0200

    rendu maquette

--- scripts/factories/alertnorecipe.js
+++ scripts/factories/alertnorecipe.js
@@ -1,11 +1,3 @@
-// export function buildHeader(){
-// const header = document.createElement('header');
-// header.className = 'header_page container'
-// header.innerHTML = `
-// <img src="./assets/logo_toque.png" alt="logo toque de cuisinier" class="d-block mx-auto logo_toque">
-// <h1 class=""><img class= "d-block mx-auto" src="./assets/Les petits plats.png" alt="Les petits plats"></h1>`
-// return header;
-// }
 export let isAlert;
 
 export function noRecipeAlert(){
