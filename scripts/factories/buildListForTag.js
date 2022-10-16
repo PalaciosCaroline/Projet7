237531660befd124834f449f71851638496707a5
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-16 22:10:15 +0200

    gestion du placeholder des listes de tag

--- scripts/factories/buildListForTag.js
+++ scripts/factories/buildListForTag.js
@@ -7,7 +7,7 @@ const inputList = document.querySelectorAll('.inputList');
 function CapitlizeString(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
+   
 export function buildUlListfilter(ArrayList, containerList) {
     containerList.innerHTML = '';
     ArrayList.forEach((item => {
@@ -38,21 +38,32 @@ function openListTagChoice(itemOpen, indexOpen, itemclose1, indexclose1, itemclo
 }
 
 function closeListTagChoice(item, index){
-     boxresults[index].querySelector('input').value = '';
+    boxresults[index].querySelector('input').value = '';
     chevronDown(item);
     btnList[index].classList.remove('isOpen');
     boxresults[index].classList.remove('open');
+    if (index == 0){
+        inputList[0].placeholder = 'Ingrédients';
+    } else if (index == 1){
+        inputList[1].placeholder = 'Appliance';
+    } else if (index == 2){
+        inputList[2].placeholder = 'Ustensils';
+    }
 }
 
 export function getOpenListTag() {
+
     btnList.forEach(item => item.addEventListener('click', () => {
         if(!item.classList.contains('isOpen')){
             item.classList.add('isOpen');
             if(item.classList.contains('btn_ingredients')){
+                inputList[0].placeholder = 'Rechercher un ingrédient';
                     openListTagChoice(icons[0], 0, icons[1], 1, icons[2], 2);
             }else if(item.classList.contains('btn_appliance')){
+                inputList[1].placeholder = 'Rechercher un appareil';
                     openListTagChoice(icons[1], 1, icons[0], 0, icons[2], 2);
             } else if(item.classList.contains('btn_ustensils')){
+                inputList[2].placeholder = 'Rechercher un ustensil';
                     openListTagChoice(icons[2], 2, icons[1], 1, icons[0], 0);
             }
         } else if(item.classList.contains('isOpen')){
@@ -71,12 +82,15 @@ export function getOpenListTag() {
 
     inputList.forEach(item => item.addEventListener('focusin', (e) => {
         if(e.target.parentNode.classList.contains('label_ingredients')){
+            inputList[0].placeholder = 'Rechercher un ingrédient';
             btnList[0].classList.add('isOpen');
             openListTagChoice(icons[0], 0, icons[1], 1,icons[2], 2);
         } else if(e.target.parentNode.classList.contains('label_appliance')) {
+            inputList[1].placeholder = 'Rechercher un appareil';
             btnList[1].classList.add('isOpen');
             openListTagChoice(icons[1], 1,icons[0], 0,icons[2], 2);
         } else if(e.target.parentNode.classList.contains('label_ustensils')) {
+            inputList[2].placeholder = 'Rechercher un ustensil';
             btnList[2].classList.add('isOpen');
             openListTagChoice(icons[2], 2, icons[1], 1, icons[0], 0);
         }
