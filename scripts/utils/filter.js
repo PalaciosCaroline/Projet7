accfe52079c9c45677459651b9f234745dd822ef
PalaciosCaroline <caroline2807@yahoo.fr>
2022-10-16 21:17:58 +0200

    nettoyage

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -170,8 +170,10 @@ function searchByTag() {
 
 //function intermédiaire de recherche searchbar
 function ingredientIsHere(recipe, research){
-    recipe.ingredients.filter(item =>
-        (getStringForCompare(item.ingredient).includes(research)).length > 0); 
+    if(recipe.ingredients.filter(item =>
+        getStringForCompare(item.ingredient).includes(research)).length > 0){
+        return true;
+    } else return false; 
 }
 
 //function intermédiaire de recherche searchbar
